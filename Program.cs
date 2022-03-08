6bf130c59f1a76ae9d03c4ef8f6a94c8be80b75d
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-08 17:27:13 +0530

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkedListSolu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int Choice,data,p;
+
+            Console.WriteLine("Welcome to Linked List operation");
+            Console.ReadKey();
+        }
+    }
+}
